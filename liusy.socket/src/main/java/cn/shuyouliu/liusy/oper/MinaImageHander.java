cea4c1babb51fffd129672fd2eb2d318f93cae8d
shuyou.liu <shuyou.liu@zymobi.com>
2016-04-28 10:38:56 +0800

    准备 socket 通讯资料

--- liusy.socket/src/main/java/cn/shuyouliu/liusy/oper/MinaImageHander.java
+++ liusy.socket/src/main/java/cn/shuyouliu/liusy/oper/MinaImageHander.java
@@ -10,45 +10,49 @@ import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.service.IoHandler;
 import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.core.session.IoSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import cn.shuyouliu.liusy.LiusySocketServer;
 import cn.shuyouliu.liusy.entity.Message;
 
 public class MinaImageHander implements IoHandler {
-
+	private static final Logger logger = LoggerFactory.getLogger(MinaImageHander.class);
+	
 	@Override
 	public void exceptionCaught(IoSession arg0, Throwable arg1) throws Exception {
 		arg1.printStackTrace();
-
+		logger.debug(" exceptionCaught arg0"+arg0+"arg1"+arg1);
 	}
 
 	@Override
 	public void messageReceived(IoSession arg0, Object arg1) throws Exception {
 		// TODO Auto-generated method stub
-
+		logger.debug(" messageReceived arg0"+arg0+"arg1"+arg1);
 	}
 
 	@Override
 	public void messageSent(IoSession arg0, Object arg1) throws Exception {
 		// TODO Auto-generated method stub
-
+		logger.debug(" messageSent arg0"+arg0+"arg1"+arg1);
 	}
 
 	@Override
 	public void sessionClosed(IoSession arg0) throws Exception {
 		// TODO Auto-generated method stub
-
+		logger.debug(" sessionClosed arg0"+arg0);
 	}
 
 	@Override
 	public void sessionCreated(IoSession arg0) throws Exception {
 		// TODO Auto-generated method stub
-
+		logger.debug(" sessionCreated arg0"+arg0);
 	}
 
 	@Override
 	public void sessionIdle(IoSession arg0, IdleStatus arg1) throws Exception {
 		// TODO Auto-generated method stub
-
+		logger.debug(" sessionIdle arg0"+arg0);
 	}
 
 	@Override
